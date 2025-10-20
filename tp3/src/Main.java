f8b1453beab1166c38b63e7105b02e89c2da055e
22205858 <kevin.kolosiana@gmail.com>
2025-10-20 16:25:59 +0200

    Tests des classes BAL, Producer, Consumer et ajout des out

--- tp3/src/Main.java
+++ tp3/src/Main.java
@@ -2,9 +2,45 @@ package tp3;
 
 public class Main {
     public static void main(String[] args) {
+        // Création de la BAL partagée
         BAL bal = new BAL();
 
-        new Producer(bal).start();
-        new Consumer(bal).start();
+        // Création des producteurs
+        Producer producer1 = new Producer(bal);
+        Producer producer2 = new Producer(bal);
+
+        // Création des consommateurs
+        Consumer consumer1 = new Consumer(bal);
+        Consumer consumer2 = new Consumer(bal);
+
+        // Démarrage des threads
+        producer1.start();
+        producer2.start();
+        consumer1.start();
+        consumer2.start();
+
+        // Laisser tourner le système pendant 10 secondes
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Arrêt propre des threads
+        producer1.interrupt();
+        producer2.interrupt();
+        consumer1.interrupt();
+        consumer2.interrupt();
+
+        try {
+            producer1.join();
+            producer2.join();
+            consumer1.join();
+            consumer2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Simulation terminée.");
     }
 }
